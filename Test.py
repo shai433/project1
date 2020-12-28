80e3258fce7a5eb7b1c01645ae835eff91bff954
shai433 <development.workshop.shai@gmail.com>
2020-12-28 20:46:58 +0200

    added a jupyter notebook file

---
+++ Test.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+print('Hello')
+print('NewLine')
+
+
+# In[11]:
+
+
+x = 10
+
+
+# In[12]:
+
+
+print(x)
+
+
+# # This is our variable x multiplied by eight
+
+# In[ ]:
+
+
+print(x * 8)
+
+
+# In[14]:
+
+
+print(x + 15)
+
+
+# In[16]:
+
+
+print(10+3)
+
+
+# In[ ]:
+
+
+
+
